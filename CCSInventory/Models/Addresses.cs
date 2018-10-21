feda3e813697d33e6a4d825b9cec4c9705f1b8a2
kshamsa <kionkshamsa@gmail.com>
2018-10-21 16:01:38 -0600

    Added Models
    the models added were:
    -Addresses
    -Donors
    -Donations
    -DonationLineItems

---
+++ CCSInventory/Models/Addresses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSInventory.Models
+{
+    public class Addresses
+    {
+        public long Id { get; set; }
+
+        public string StreetAddress1 { get; set; }
+        public string StreedAddress2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ CCSInventory/Models/DonationLineItems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSInventory.Models
+{
+    public class DonationLineItems
+    {
+        public int Id { get; set; }
+        public long DonationId { get; set; }
+        public long TypeId { get; set; }
+        public decimal Weight { get; set; }
+        public bool Taxable { get; set; }
+        public bool USDA { get; set; }
+        public String Note { get; set; }
+    }
+}
