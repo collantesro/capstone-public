3e68674f61c185ae02ad6967e46c0609ef63d5a0
Amy Lea <amylea@mail.weber.edu>
2018-10-23 20:01:39 -0600

    Created the Recipient Model

---
+++ CCSInventory/Models/Recipient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSInventory.Models
+{
+    public class Recipient : TrackedModel
+    {
+    public int Id { get; set; }
+    public int DonorId { get; set; }
+    public int USDACategoryId { get; set; }
+    public int TypeId { get; set; }
+    public int CategoryId { get; set; }
+    public string RecipientName { get; set; }
+    public string Notes { get; set; }
+    }
+}
+
+
