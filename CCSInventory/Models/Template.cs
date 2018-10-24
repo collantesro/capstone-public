c6d45796395188c3eae40982d24be6f997fdecdc
Amy Lea <amylea@mail.weber.edu>
2018-10-23 20:14:21 -0600

    Created the Template Model

---
+++ CCSInventory/Models/Template.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSInventory.Models
+{
+    public class Template : TrackedModel
+    {
+        public int Id { get; set; }
+        public string TemplateName { get; set; }
+        public DateTime TemplateDate { get; set; }
+        public int TemplateType { get; set; }
+
+    }
+}
