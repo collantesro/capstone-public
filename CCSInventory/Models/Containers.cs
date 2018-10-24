2e733b5b2070a0266df9ab3b22f17ef6fbe46307
Amy Lea <amylea@mail.weber.edu>
2018-10-23 19:50:54 -0600

    Made changes to the Container Model

--- CCSInventory/Models/Containers.cs
+++ CCSInventory/Models/Containers.cs
@@ -2,22 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace CCSInventory.Models
 {
-    public class Containers
+    public class Container : TrackedModel 
+    // Extend from TrackedModel (Also singularly named, "Containers" => "Container")
     {
-        public int Id{ get; set; }
+        public int Id { get; set; }
         public int BinNumber { get; set; }
         public int USDACategoryId { get; set; }
         public int TypeId { get; set; }
         public bool USDA { get; set; }
         public decimal Weight { get; set; }
         public int Cases { get; set; }
-        public string CreatedBy { get; set; }
-        public DateTime Created { get; set; }
-        public string ModifiedBy { get; set; }
-        public DateTime Modified { get; set; }
 
+        // This means you don't specify Created, Modified, CreatedBy, and ModifiedBy.
+        // CCSDbContext will automatically handle values for Created and Modified as long as you extend from TrackedModel.
     }
 }
