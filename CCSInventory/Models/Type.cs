f66c8760c6821238490b17cc7ae08d90304dfa41
Amy Lea <amylea@mail.weber.edu>
2018-10-23 20:05:50 -0600

    Created the Type Model

---
+++ CCSInventory/Models/Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSInventory.Models
+{
+    public class Type
+    {
+        public int Id { get; set; }
+        public int ContainerId { get; set; }
+        public int RecipientId { get; set; }
+        public string TypeName { get; set; }
+    }
+}
