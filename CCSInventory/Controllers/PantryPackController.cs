02f5b4dd3406c63e996aece55b6d940cc308077b
kshamsa <kionkshamsa@gmail.com>
2018-10-25 18:58:59 -0600

    worked on pantry pack
    I got some of pantry pack started, something came up and I will finish it in the morning. Just wanted to commit what I have now.

---
+++ CCSInventory/Controllers/PantryPackController.cs
@@ -0,0 +1,46 @@
+﻿using CCSInventory.Models;
+using CCSInventory.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSInventory.Controllers
+{
+    public class PantryPackController : Controller
+    {
+
+        private CCSDbContext _context; 
+
+        public PantryPackController(CCSDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult LoadTransactionPage()
+        {
+            return View("Transaction");
+        }
+
+        public IActionResult LoadAllTransactionPage()
+        {
+            return View("AllTransactions");
+        }
+
+        public IActionResult LoadOutGoingTransactionPage()
+        {
+            return View("OutGoingTransactions");
+        }
+
+        public void OutGoingTransaction(PantryPackTransactionType transaction)
+        {
+            PantryPackTransactionType newTransaction = new PantryPackTransactionType();
+
+            newTransaction.Qty = transaction.Qty;
+            newTransaction.PackType = transaction.PackType; 
+
+
+        }
+    }
+}
---
+++ CCSInventory/Models/ViewModels/PantryPackTransactionType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSInventory.Models.ViewModels
+{
+    public class PantryPackTransactionType : TrackedModel
+    {
+        public int TypeID { get; set; }
+        public int TransactionID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Note { get; set; }
+
+        public int Qty { get; set; }
+        public int PackTypeID { get; set; }
+        public PantryPackType PackType { get; set; }
+    }
+}
